7efb6d277391fa2a1a3719babef63ba94e1df404
Miguel Fernandez <miferna@microsoft.com>
2020-03-06 18:10:03 -0600

    initial

---
+++ GovFunc/startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation.ApplicationId;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+[assembly: FunctionsStartup(typeof(MyNamespace.Startup))]
+
+namespace MyNamespace
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var quickPulseFactory = builder.Services.FirstOrDefault(sd => sd.ServiceType == typeof(ITelemetryModule) &&
+                                                 sd.ImplementationType == typeof(QuickPulseTelemetryModule));
+            if (quickPulseFactory != null)
+            {
+                builder.Services.Remove(quickPulseFactory);
+            }
+
+            var appIdFactory = builder.Services.FirstOrDefault(sd => sd.ServiceType == typeof(IApplicationIdProvider));
+            if (appIdFactory != null)
+            {
+                builder.Services.Remove(appIdFactory);
+            }
+
+            var channelFactory = builder.Services.FirstOrDefault(sd => sd.ServiceType == typeof(ITelemetryChannel));
+            if (channelFactory != null)
+            {
+                builder.Services.Remove(channelFactory);
+            }
+
+            builder.Services.AddSingleton<ITelemetryModule, QuickPulseTelemetryModule>(_ =>
+                new QuickPulseTelemetryModule
+                {
+                    QuickPulseServiceEndpoint = "https://quickpulse.applicationinsights.us/QuickPulseService.svc"
+                });
+
+            builder.Services.AddSingleton<IApplicationIdProvider, ApplicationInsightsApplicationIdProvider>(_ => new ApplicationInsightsApplicationIdProvider() { ProfileQueryEndpoint = "https://dc.applicationinsights.us/api/profiles/{0}/appId" });
+
+            builder.Services.AddSingleton<ITelemetryChannel>(_ => new ServerTelemetryChannel() { EndpointAddress = "https://dc.applicationinsights.us/v2/track" });
+        }
+    }
+}
